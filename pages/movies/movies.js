1bb9510319d9b305e12276b7e0c95bc7c65c142b
yang <Cadenyang5255@163.com>
2019-05-16 18:55:34 +0800

    增加：电影列表，从豆瓣获取数据

---
+++ pages/movies/movies.js
@@ -0,0 +1,60 @@
+// pages/movies/movies.js
+var app = getApp();
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        inTheaters: {},
+        comingSoon: {},
+        top250: {},
+    },
+    onLoad: function (options) {
+        var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=5&count=3"
+        var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3"
+        var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3"
+
+        this.getMovieListData(inTheatersUrl, 'inTheaters', '正在热映')
+        this.getMovieListData(comingSoonUrl, 'comingSoon', '即将上映')
+        this.getMovieListData(top250Url, 'top250', '豆瓣Top250')
+    },
+    getMovieListData(url, settedkey, categoryTitle) {
+        var that = this
+        wx.request({
+            url: url,
+            method: 'GET',
+            header: {
+                'Content-Type': 'json'
+            },
+            success(res) {
+                console.log(res)
+                that.processMovieData(res.data.subjects, settedkey, categoryTitle)
+            }
+        })
+    },
+
+    processMovieData(moviesDouban, settedkey, categoryTitle) {
+        var movies = []
+        for (var index in moviesDouban){
+            var subject = moviesDouban[index]
+            var title = subject.title
+            if (title.length > 6){
+                title = title.substring(0,6) + "..."
+            }
+            var temp = {
+                title: title,
+                average: subject.rating.average,
+                coverageUrl: subject.images.large,
+                movieId: subject.id
+            }
+            movies.push(temp)
+        }
+        var readyData = {}
+        readyData[settedkey] = {
+            categoryTitle: categoryTitle,
+            movies: movies
+        }
+        this.setData(readyData)
+    } 
+})
